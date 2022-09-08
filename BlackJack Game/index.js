fa63e264c9c81b381c1a688f75881f7a60a6e24f
Achal <achal.ch@gmail.com>
2022-09-09 01:04:24 +0530

    update

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -5,10 +5,10 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-// let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el");
+let cardEl = document.querySelector("#card-el");
 
-function startGame() {
+function renderGame() {
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -20,4 +20,10 @@ function startGame() {
   }
   messageEl.textContent = message
   sumEl.textContent = "Sum: " + sum;
+  cardEl.textContent = "Cards: " + firstCard + " " + secondCard;
+}
+function newCard(){
+    let newCard = 10;
+    sum += newCard;
+    renderGame();
 }
