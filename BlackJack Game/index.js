5afef755e5eea3b4bfa227b5bcab7a9e44433d2b
Achal <achal.ch@gmail.com>
2022-09-09 00:13:06 +0530

    Update

---
+++ BlackJack Game/index.js
@@ -0,0 +1,23 @@
+let firstCard = 5;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message
+  sumEl.textContent = "Sum: " + sum;
+}
